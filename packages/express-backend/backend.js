0671ad3efa28c965debf9d714b3af68a5ad2c29c
Sohini-pillay <sohinispillay@gmail.com>
2024-01-31 22:09:18 -0800

    status tested

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -96,7 +96,7 @@ app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
   if (result === undefined) {
-    res.status(404).send("Resource not found.");
+    res.status(201).send("Resource not found.");
   } else {
     res.send();
   }
@@ -107,7 +107,7 @@ app.post("/users", (req, res) => {
   userToAdd.id = generateID();
 
   addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  res.status(404).send(userToAdd);
 });
 
 app.delete("/users/:id", (req, res) => {
